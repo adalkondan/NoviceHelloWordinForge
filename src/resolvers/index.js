d9051ddc1caff91e5bc992bb6dee64d35b471b54
Koushik Sampath <01101998skk@gmail.com>
2025-06-01 01:16:02 +0530

    first commit

---
+++ src/resolvers/index.js
@@ -0,0 +1,11 @@
+import Resolver from '@forge/resolver';
+
+const resolver = new Resolver();
+
+resolver.define('getText', (req) => {
+  console.log(req);
+
+  return 'Welcome to the ' + req.context.extension.space.key + ' space!';
+});
+
+export const handler = resolver.getDefinitions();
